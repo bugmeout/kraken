fd00c7513adbd66968659f5280d8acfccc8b258e
Thomas Chopitea <tomchop@gmail.com>
2015-05-29 15:14:44 +0200

    Version information

---
+++ kraken-agent/agent/bin/versioninfo.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(1, 0, 0, 0),
+    prodvers=(1, 0, 0, 0),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'CERT SG'),
+        StringStruct(u'FileDescription', u'Kraken'),
+        StringStruct(u'FileVersion', u'1.0.0.0'),
+        StringStruct(u'InternalName', u'kraken'),
+        StringStruct(u'LegalCopyright', u'\xa9 CERT Societe Generale. All rights reserved.'),
+        StringStruct(u'OriginalFilename', u'kraken.exe'),
+        StringStruct(u'ProductName', u'Microsoft\xae Windows\xae Operating System'),
+        StringStruct(u'ProductVersion', u'1.0.0.0')])
+      ]), 
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)
